63fe91958ad47addf8073e34d70d3ca50c4323b9
farhan.huda <farhan.huda@pactindo.com>
2024-08-27 09:58:05 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1>Hello World!</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-textarea variant="outlined" label="Input Raw" hide-details v-model="inputRaw"></v-textarea>
+        <label v-if="inputRaw.length > 0">{{ inputRaw.length }}</label>
+      </v-col>
+    </v-row>
+    <mode-one :data="data" :inputRaw="inputRaw"></mode-one>
+  </v-container>
+</template>
+<script>
+import ModeOne from './components/ModeOne.vue';
+
+export default {
+  components: {
+    ModeOne
+  },
+  data() {
+    return {
+      inputRaw: "",
+      mode: ["mode1", "mode2", "mode3"],
+      start: 0,
+      end: 0,
+      result: "",
+      data: ""
+    };
+  },
+};
+</script>
---
+++ src/components/ModeOne.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="mode-one">
+    <v-row>
+      <v-col cols="12">
+        <h2>The character you want to take</h2>
+      </v-col>
+    </v-row>
+    <v-row class="text-center align-center">
+      <v-col cols="5">
+        <v-text-field variant="outlined" label="Start" hide-details type="number" v-model="start"></v-text-field>
+      </v-col>
+      <v-col cols="2">
+        <label>to</label>
+      </v-col>
+      <v-col cols="5">
+        <v-text-field variant="outlined" label="End" hide-details type="number" v-model="end"></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-textarea variant="outlined" label="Result" readonly hide-details v-model="result"></v-textarea>
+        <label v-if="result.length > 0">{{ result.length }}</label>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModeOne",
+  props: ["data", "inputRaw"],
+  data() {
+    return {
+      start: 0,
+      end: 0,
+      result: ""
+    };
+  },
+  watch: {
+    start: function(newVal) {
+      this.result = this.inputRaw.slice(newVal, this.end);
+    },
+    end: function(newVal) {
+      console.log(this.inputRaw)
+      this.result = this.inputRaw.slice(this.start, newVal);
+    },
+    inputRaw: function() {
+      this.result = this.inputRaw.slice(this.start, this.end);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
